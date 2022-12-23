c34fc8583f7aec1988831a334b90dac7a1ca0d3d
rp246c6 <91663071+rp246c6@users.noreply.github.com>
2022-12-23 09:27:08 +0000

    code

---
+++ src/main/java/com/maven/demo/App.java
@@ -0,0 +1,20 @@
+package com.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+/*echo "# github-actions-example" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rp246c6/github-actions-example.git
+git push -u origin main*/
